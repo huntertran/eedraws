a3871db60e231f3c322f009f0df9f34dbb5e8562
Hunter Tran <4468525+huntertran@users.noreply.github.com>
2024-06-19 00:34:14 -0400

    draw heatmap

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 async function start() {
     const data = await fetchDataAndStore();
-    draw(prepareData(data));
+    const preparedData = prepareData(data);
+    draw(preparedData);
+    drawScoreDistributionsHeatmap(preparedData);
     drawLastScoreContribution(data.rounds[0]);
     ko.applyBindings(new LastRoundViewModel(data.rounds[0]));
 }
@@ -17,19 +19,48 @@ async function fetchDataAndStore() {
 function prepareData(data) {
     let x = [];
     let y = [];
+    let z = [];
+    let text = [];
     for (let i = 0; i < data.rounds.length; i++) {
         x.push(data.rounds[i].drawDate);
-        y.push(data.rounds[i].drawCRS);
+        y.push(parseInt(data.rounds[i].drawCRS));
+        z.push([
+            parseInt(data.rounds[i].dd1.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd2.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd4.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd5.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd6.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd7.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd8.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd10.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd11.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd12.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd13.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd14.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd15.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd16.replace(/,/g, ''), 10),
+            parseInt(data.rounds[i].dd17.replace(/,/g, ''), 10)
+        ]);
+        text.push(data.rounds[i].drawCRS);
     }
-    return { x, y, type: 'scatter' };
+
+    return { x, y, z, text };
 }
 
 function draw(data) {
     eeDraws = document.getElementById('ee-draws');
 
-    data.line = {
-        shape: 'spline',
-        smoothing: 0.7,
+    let drawData = {
+        x: data.x,
+        y: data.y,
+        text: data.text,
+        line: {
+            shape: 'spline',
+            smoothing: 0.7,
+        },
+        type: 'scattergl',
+        mode: 'lines+markers+text',
+        textposition: 'top center',
     }
 
     var today = new Date();
@@ -76,33 +107,33 @@ function draw(data) {
         responsive: true
     };
 
-    Plotly.newPlot(eeDraws, [data], layout, config);
+    Plotly.newPlot(eeDraws, [drawData], layout, config);
 }
 
 function drawLastScoreContribution(data) {
-
     let myData = [
         {
-            x: ["dd1", "dd2", "dd3", "dd4", "dd5", "dd6", "dd7", "dd8", "dd9", "dd10", "dd11", "dd12", "dd13", "dd14", "dd15", "dd16", "dd17"],
-            y: [data.dd1, data.dd2, data.dd3, data.dd4, data.dd5, data.dd6, data.dd7, data.dd8, data.dd9, data.dd10, data.dd11, data.dd12, data.dd13, data.dd14, data.dd15, data.dd16, data.dd17],
-            type: 'bar'
+            x: ["601-1200", "501-600", "491-500", "481-490", "471-480", "461-470", "451-460", "441-450", "431-440", "421-430", "411-420", "401-410", "351-400", "301-350", "0-300"],
+            y: [data.dd1, data.dd2, data.dd4, data.dd5, data.dd6, data.dd7, data.dd8, data.dd10, data.dd11, data.dd12, data.dd13, data.dd14, data.dd15, data.dd16, data.dd17],
+            type: 'bar',
+            text: [data.dd1, data.dd2, data.dd4, data.dd5, data.dd6, data.dd7, data.dd8, data.dd10, data.dd11, data.dd12, data.dd13, data.dd14, data.dd15, data.dd16, data.dd17],
+            textposition: 'auto',
         }
     ];
 
     var layout = {
         margin: { t: 0 },
-        autosize: true,
         xaxis: {
-            autorange: false,
+            autorange: true,
             title: {
                 text: 'Distribution'
             },
         },
         yaxis: {
+            autorange: true,
             title: {
                 text: 'CRS Score'
             },
-            // range: [0, Math.max(...myData[0].y)]
         }
     };
 
@@ -111,6 +142,41 @@ function drawLastScoreContribution(data) {
     };
 
     Plotly.newPlot('score-distribution', myData, layout, config);
+}
+
+function drawScoreDistributionsHeatmap(data) {
+    let drawData = [{
+        x: data.x,
+        y: ["601-1200", "501-600", "491-500", "481-490", "471-480", "461-470", "451-460", "441-450", "431-440", "421-430", "411-420", "401-410", "351-400", "301-350", "0-300"],
+        z: data.z,
+        text: data.z,
+        textposition: 'auto',
+        type: 'heatmap',
+        // colorscale: 'Viridis',
+        showscale: true
+    }];
+
+    var config = {
+        responsive: true
+    };
+
+    var testData1 = [
+        {
+            z: [[1, 20, 30], [20, 1, 60], [30, 60, 1]],
+            type: 'heatmap'
+        }
+    ];
+
+    var testData = [
+        {
+            z: data.z,
+            type: 'heatmap'
+        }
+    ];
+
+    // Plotly.newPlot('score-distributions-heatmap', data, layout, config);
+    Plotly.newPlot('score-distributions-heatmap', drawData);
+
 }
 
 function LastRoundViewModel(lastRound) {
